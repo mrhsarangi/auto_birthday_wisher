4feba4f16ff0c918d0185554569268ea6ea6bc65
mrhsarangi <1920hrud@gmail.com>
2024-06-18 00:18:14 +0530

    Completed upto sending emails, now all that left is to create some birthday templates and then choosing one randomly and replacing recipients name and sending it.

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
-import requests
-import datetime 
+import smtplib
+from datetime import date
+from random import randint
+from email.mime.text import MIMEText
 
 
 # CONSTANTS
 FILEPATH = "data/data.csv"
+PORT = 465
+SMTP_SERVER = "smtp.gmail.com"
+SENDER_EMAIL = "1920hrud@gmail.com"
+PASSWORD = "aqup dmze iyiv fthu"
 
-
-def load_data(filepath: str) -> list[list]:
+def load_data(filepath: str) -> list[tuple]:
+    data = []
     with open(filepath) as f:
-        header = f.readline()
+        header = [ s.strip() for s in f.readline().split(',')]
         print(header)
         for line in f.readlines():
-            print(line) 
+            name, dob, email = [s.strip() for s in line.split(',')]
+            data.append((name, dob, email))
 
-    return [[]]
+    return data
+
+
+def send_wish(name: str, email: str) -> None:
+    test_msg = "Test Mail"
+    try:
+        with smtplib.SMTP_SSL(SMTP_SERVER, PORT) as smtp_server:
+            smtp_server.ehlo()
+            smtp_server.login(SENDER_EMAIL, PASSWORD)
+            smtp_server.sendmail(SENDER_EMAIL, email, test_msg)
+        print(f'Wish sent to {name} at {email}')
+    except Exception as e:
+        print("Exception encountered:\n", e)
+        
 
 
 if __name__ == "__main__":
     data = load_data(FILEPATH)
 
+    today = str(date.today())
+    year, month, day = [s.strip() for s in today.split('-')]
+    
+    for name, dob, email in data[:]:
+        # convert dob to int and then to a date object
+        birth_month, birth_day = dob.split('-')
+        if month == birth_month and day == birth_day:
+            print("Sending mail")
+            send_wish(name, email)
 
+    exit(0)
